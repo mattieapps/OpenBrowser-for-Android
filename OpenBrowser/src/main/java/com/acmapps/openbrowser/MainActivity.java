2b7a8fa3f0e65ba466ae20f03bc5cf28e80994d3
acmlabs <acmlabs@users.noreply.github.com>
2013-12-16 12:14:46 -0500

    Organized Imports

--- OpenBrowser/src/main/java/com/acmapps/openbrowser/MainActivity.java
+++ OpenBrowser/src/main/java/com/acmapps/openbrowser/MainActivity.java
@@ -1,16 +1,10 @@
 package com.acmapps.openbrowser;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.webkit.WebView;
+import android.support.v7.app.*;
+import android.support.v4.app.*;
+import android.view.*;
+import android.os.*;
+import android.webkit.*;
 
 public class MainActivity extends ActionBarActivity {
 
