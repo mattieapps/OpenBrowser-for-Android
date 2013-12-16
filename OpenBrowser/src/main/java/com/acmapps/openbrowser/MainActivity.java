4497b8ee6f293a8a510c8966fd826d7e0f14b361
Andrew Mattie <appsacm@gmail.com>
2013-12-15 23:39:12 -0500

    Fixed Layout

--- OpenBrowser/src/main/java/com/acmapps/openbrowser/MainActivity.java
+++ OpenBrowser/src/main/java/com/acmapps/openbrowser/MainActivity.java
@@ -17,6 +17,12 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new DummyFragment())
+                    .commit();
+        }
     }
 
 
@@ -39,4 +45,21 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    /**
+     * A dummy fragment containing a simple view.
+     */
+    public static class DummyFragment extends Fragment {
+
+        public DummyFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            return rootView;
+        }
+    }
+
 }
