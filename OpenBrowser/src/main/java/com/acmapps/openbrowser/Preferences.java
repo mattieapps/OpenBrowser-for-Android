4583b30ceb26bd1d70324d82c79a725247e12252
Andrew Mattie <appsacm@gmail.com>
2013-12-21 00:02:24 -0500

    Added Empty Preferences Activity

---
+++ OpenBrowser/src/main/java/com/acmapps/openbrowser/Preferences.java
@@ -0,0 +1,14 @@
+package com.acmapps.openbrowser;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Created by andrewmattie on 12/20/13.
+ */
+public class Preferences extends PreferenceActivity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
