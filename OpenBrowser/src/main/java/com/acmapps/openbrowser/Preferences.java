66d19af97a6a198fdcf7ee95812245cb09a3d5e5
Andrew Mattie <appsacm@gmail.com>
2013-12-22 03:27:24 -0500

    Added Code for preferences layout

--- OpenBrowser/src/main/java/com/acmapps/openbrowser/Preferences.java
+++ OpenBrowser/src/main/java/com/acmapps/openbrowser/Preferences.java
@@ -1,14 +1,17 @@
 package com.acmapps.openbrowser;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 /**
  * Created by andrewmattie on 12/20/13.
  */
+@SuppressWarnings("deprecation")
 public class Preferences extends PreferenceActivity {
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        super.setTitle("Settings");
+        addPreferencesFromResource(R.xml.settings);
     }
 }
